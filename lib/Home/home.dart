9010dc772102b4c1074702b7d88b18f0c6c990bc
Saad Saleem <msaadsaleem1001@gmail.com>
2024-02-27 21:36:16 +0500

    second commit

--- lib/Home/home.dart
+++ lib/Home/home.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 
 class HiveHome extends StatefulWidget {
   const HiveHome({super.key});
@@ -20,7 +21,25 @@ class _HiveHomeState extends State<HiveHome> {
       body: SafeArea(
           child: Column(
         children: [
+          FutureBuilder(
+              future: Hive.openBox('Info'),
+              builder: (context, snapshot){
+                return Column(
+                  children: [
+                    Text(snapshot.data!.get('name').toString()),
+                    Text(snapshot.data!.get('prof').toString()),
 
+                    IconButton(onPressed: (){
+                      snapshot.data!.put('name', 'Saad Saleem');
+                      snapshot.data!.put('prof', 'Flutter Developer');
+                      setState(() {
+
+                      });
+                    }, icon: const Icon(Icons.add))
+                  ],
+                );
+              }
+          )
         ],
       )),
     );
